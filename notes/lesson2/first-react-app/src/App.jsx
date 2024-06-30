da5f7f3a7449201dfc2c5645fefa80a595556d03
Beibarys <johannneumann72@gmail.com>
2024-07-01 00:15:26 +0500

    add Die template code

--- notes/lesson2/first-react-app/src/App.jsx
+++ notes/lesson2/first-react-app/src/App.jsx
@@ -4,14 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Chicken from './Chicken'
 import Greeter from './Greeter'
+import Die from './Die'
 
 function App() {
   return (
+    <div>
+      <Die />
+      <Die />
+      <Die />
+    </div>
+    /*
     <div>
       <Greeter person="Naruto" from="Jiraya"/>
       <Greeter person="Sakura" from="Tsunade"/>
       <Greeter person="Sasuke" from="Orochimaru"/>
     </div>
+    */
   );
 }
 
