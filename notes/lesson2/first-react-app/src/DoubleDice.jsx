37db892c2f47d00698e32c7cf25112aaa1bc96bd
Beibarys <johannneumann72@gmail.com>
2024-07-01 22:32:03 +0500

    style

--- notes/lesson2/first-react-app/src/DoubleDice.jsx
+++ notes/lesson2/first-react-app/src/DoubleDice.jsx
@@ -3,11 +3,11 @@ export default function DoubleDice(){
     const num2 = Math.floor(Math.random() * 3) + 1;
 
     // const result = num1 === num2 ? "You win xD" : "You lose :(";
-
+    const styles = {color: num1 === num2 ? "green" : "red"};
     return (
-        <div>
-            <h2>Double Dice</h2>
-            {num1 === num2 ? <h3>"You win"</h3> : null}
+        <div className="DoubleDice">
+            <h2 style={styles}>Double Dice</h2>
+            {num1 === num2 && <h3>"You win"</h3>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
